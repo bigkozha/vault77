bed7b50ff9237374fe91d5731a70cfd1fccb9b29
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-10 21:13:18 +0600

    initial commit

---
+++ 1_selenium/1_selenium/task1.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+LINL_CLASS_NAME = '.LC20lb.DKV0Md'
+QUERY_WORD = 'nuraly zhanbyrbayev'
+
+driver = webdriver.Remote(command_executor='127.0.0.1:9515')
+driver.get('https://google.kz')
+assert 'Google' in driver.title
+
+#get search results
+elementQ = driver.find_element_by_name('q')
+elementQ.send_keys(QUERY_WORD)
+elementQ.send_keys(Keys.RETURN)
+
+#get first 5 links text 
+first_five_links = driver.find_elements_by_css_selector(LINL_CLASS_NAME)[:5]
+assert(len(first_five_links) == 5)
+assert(None not in first_five_links)
+
+for link in first_five_links:
+    print(link.text)
+
+driver.close()
