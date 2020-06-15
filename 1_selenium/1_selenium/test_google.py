42b5c2b2c96851d58e481561bf7554a3633fd4a0
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-15 11:59:33 +0600

    add pytest and fixtures in test_google.py

--- 1_selenium/1_selenium/test_google.py
+++ 1_selenium/1_selenium/test_google.py
@@ -1,24 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-search_result_class_name = '.LC20lb.DKV0Md'
-query_word = 'nuraly zhanbyrbayev'
 
-driver = webdriver.Remote(command_executor='127.0.0.1:9515')
-driver.get('https://google.kz')
-assert 'Google' in driver.title
+def test_google(mock_tuple):
+    search_result_class_name = '.LC20lb.DKV0Md'
+    query_word = mock_tuple[0]()
 
-#get search results
-search_box = driver.find_element_by_name('q')
-search_box.send_keys(query_word)
-search_box.send_keys(Keys.RETURN)
+    driver = webdriver.Remote(command_executor='127.0.0.1:9515')
+    driver.get('https://google.kz')
+    assert 'Google' == mock_tuple[1]()
 
-#get first 5 links text 
-first_five_links = driver.find_elements_by_css_selector(search_result_class_name)[:5]
-assert(len(first_five_links) == 5)
-assert(None not in first_five_links)
+    # get search results
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys(query_word)
+    search_box.send_keys(Keys.RETURN)
 
-for link in first_five_links:
-    print(link.text)
+    # get first 5 links text
+    first_five_links = mock_tuple[2]()[:5]
+    assert(len(first_five_links) == 5)
+    assert(None not in first_five_links)
 
-driver.close()
+    driver.close()
