30defde5b0bb61b48e190fb72fe16da6f3022163
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-15 15:28:49 +0600

    removed tuple from test

--- 1_selenium/1_selenium/conftest.py
+++ 1_selenium/1_selenium/conftest.py
@@ -2,17 +2,15 @@ import pytest
 
 
 @pytest.fixture
-def mock_tuple():
-    return (mock_query_word, mock_title_google, mock_search_result)
-
-
 def mock_query_word():
     return 'nuraly zhanbyrbayev'
 
 
+@pytest.fixture
 def mock_title_google():
     return 'Google'
 
 
+@pytest.fixture
 def mock_search_result():
     return ['Link 1', 'Link 2', 'Link 3', 'Link 4', 'Link 5', 'Link 6']
--- 1_selenium/1_selenium/test_google.py
+++ 1_selenium/1_selenium/test_google.py
@@ -2,22 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def test_google(mock_tuple):
-    search_result_class_name = '.LC20lb.DKV0Md'
-    query_word = mock_tuple[0]()
-
+def test_google(mock_query_word, mock_title_google, mock_search_result):
+    query_word = mock_query_word
     driver = webdriver.Remote(command_executor='127.0.0.1:9515')
+
     driver.get('https://google.kz')
-    assert 'Google' == mock_tuple[1]()
+    assert 'Google' == mock_title_google
 
     # get search results
-    search_box = driver.find_element_by_name('q')
-    search_box.send_keys(query_word)
-    search_box.send_keys(Keys.RETURN)
+    search_result = driver.find_element_by_name('q')
+    search_result.send_keys(query_word)
+    search_result.send_keys(Keys.RETURN)
 
     # get first 5 links text
-    first_five_links = mock_tuple[2]()[:5]
+    first_five_links = mock_search_result[:5]
     assert(len(first_five_links) == 5)
-    assert(None not in first_five_links)
+    assert(None not in first_five_links)\
 
     driver.close()
