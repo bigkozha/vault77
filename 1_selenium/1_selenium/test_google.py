f7c4d98c44cdd877fc325a1365c600c8f26cbf33
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-15 10:37:07 +0600

    refactoring more pep8 alike

--- 1_selenium/1_selenium/test_google.py
+++ 1_selenium/1_selenium/test_google.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-LINK_CLASS_NAME = '.LC20lb.DKV0Md'
-QUERY_WORD = 'nuraly zhanbyrbayev'
+search_result_class_name = '.LC20lb.DKV0Md'
+query_word = 'nuraly zhanbyrbayev'
 
 driver = webdriver.Remote(command_executor='127.0.0.1:9515')
 driver.get('https://google.kz')
 assert 'Google' in driver.title
 
 #get search results
-elementQ = driver.find_element_by_name('q')
-elementQ.send_keys(QUERY_WORD)
-elementQ.send_keys(Keys.RETURN)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys(query_word)
+search_box.send_keys(Keys.RETURN)
 
 #get first 5 links text 
-first_five_links = driver.find_elements_by_css_selector(LINK_CLASS_NAME)[:5]
+first_five_links = driver.find_elements_by_css_selector(search_result_class_name)[:5]
 assert(len(first_five_links) == 5)
 assert(None not in first_five_links)
 
