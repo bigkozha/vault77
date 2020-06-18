85deae3dba5b2ae7ab188c287263d5cac3cc0a30
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-18 20:56:13 +0600

    add with as construction to test

--- 2_django/test_django.py
+++ 2_django/test_django.py
@@ -1,13 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import pytest
 
-def test_basic(live_server):
-    driver = webdriver.Remote(command_executor='http://127.0.0.1:9515')
-    driver.get(live_server.url)
+
+
+def test_basic(live_server, get_driver):
+    with get_driver as driver:
+        driver.get(live_server.url)
     
-    counter = driver.find_element_by_id('counter')
-    assert '1' == counter.text
-    driver.refresh()
-    counter = driver.find_element_by_id('counter')
-    assert '2' == counter.text
-    driver.close()
+        counter = driver.find_element_by_id('counter')
+        assert '1' == counter.text
+        driver.refresh()
+        counter = driver.find_element_by_id('counter')
+        assert '2' == counter.text
+        driver.close()
