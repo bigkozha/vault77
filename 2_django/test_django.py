261bdc82fa5df0a062ceea5284c11abce13ef4b8
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-18 21:03:21 +0600

    add increment test

--- 2_django/test_django.py
+++ 2_django/test_django.py
@@ -10,3 +10,9 @@ def test_basic(live_server, get_driver):
         driver.refresh()
         counter = driver.find_element_by_id('counter')
         assert '2' == counter.text
+
+def test_increment(live_server, get_driver):
+    with get_driver as driver:
+        driver.get(live_server.url + '/increment')
+        counter = driver.find_element_by_id('counter')
+        assert '2' == counter.text
