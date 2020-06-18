53264e77ffe46b00ddc5e4ccdf56fa44f874e482
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-18 22:06:22 +0600

    add decrement test

--- 2_django/test_django.py
+++ 2_django/test_django.py
@@ -20,3 +20,27 @@ def test_increment(live_server, get_driver):
         driver.get(live_server.url + '/increment')
         counter = driver.find_element_by_id('counter')
         assert '2' == counter.text
+
+
+def test_decrement(live_server, get_driver):
+    instance, created = Counter.objects.get_or_create(id=1)
+    instance.value += 1
+    instance.save()
+    assert 1 == instance.value
+
+    with get_driver as driver:
+        driver.get(live_server.url + '/decrement')
+        counter = driver.find_elememt_by_id('counter')
+        assert '0' == counter.text
+
+
+def test_decrement_instance_value_zero(live_server, get_driver):
+    instance, created = Counter.objects.get_or_create(id=1)
+    instance.value = 0
+    instance.save()
+    assert 0 == instance.value
+    
+    with get_driver as driver:
+        driver.get(live_server.url + '/decrement')
+        counter = driver.find_elememt_by_id('counter')
+        assert 'error' in live_server.url
