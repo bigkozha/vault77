be3b8d84736d5c2fb08f9e83430aee9f684b2237
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-18 20:59:21 +0600

    add scope to fixture

--- 2_django/test_django.py
+++ 2_django/test_django.py
@@ -1,14 +1,12 @@
 import pytest
 
 
-
 def test_basic(live_server, get_driver):
     with get_driver as driver:
         driver.get(live_server.url)
-    
+
         counter = driver.find_element_by_id('counter')
         assert '1' == counter.text
         driver.refresh()
         counter = driver.find_element_by_id('counter')
         assert '2' == counter.text
-        driver.close()
