6ea3bfb4c161f5f95f087156de1d198b81804bc4
n.zhanbyrbayev <n.zhanbyrbayev@tha.kz>
2020-06-22 18:43:20 +0600

    add requirements txt

--- 2_django/conftest.py
+++ 2_django/conftest.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pytest
+import os
 
 @pytest.fixture()
 def get_driver():
+    if os.environ.get('GITHUB_ACTIONS'):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Remote(command_executor='http://127.0.0.1:9515')
     yield driver
