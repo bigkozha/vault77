16fbb4defe36248d3daa919a96fe192bb0e0ae12
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-22 22:52:21 +0600

    fix typo

--- 2_django/conftest.py
+++ 2_django/conftest.py
@@ -11,6 +11,7 @@ def get_driver():
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
     else:
         driver = webdriver.Remote(command_executor='http://127.0.0.1:9515')
     yield driver
