af00bfe829d2afd7698fd7932ec8aec9658ebe61
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-28 16:28:44 +0600

    fixes

--- 2_django/conftest.py
+++ 2_django/conftest.py
@@ -1,12 +1,21 @@
 import os
 import pytest
-import pytest
 from counter.models import Counter
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.yield_fixture(scope="session")
 def driver():
-    with webdriver.Remote(command_executor='http://127.0.0.1:9515') as driver:
-        yield driver
+    if os.environ.get('GITHUB_ACTIONS'):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        with webdriver.Chrome(chrome_options=chrome_options) as driver:
+            yield driver
+    else:
+        with webdriver.Remote(command_executor='http://127.0.0.1:9515') as driver:
+            yield driver
 
 @pytest.fixture()
 def counter():
