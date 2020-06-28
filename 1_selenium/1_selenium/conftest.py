d05a5483dbb4ebd446d1f8ccffa5fbf5bf5daff1
bigkozha <nuraly.zhanbyrbayev@gmail.com>
2020-06-28 16:37:00 +0600

    fixes

--- 1_selenium/1_selenium/conftest.py
+++ 1_selenium/1_selenium/conftest.py
@@ -14,5 +14,7 @@ def expected_title_google():
 @pytest.fixture
 def expected_search_result():
     return [
-        'Nuraly Zhanbyrbayev - Kazakhstan | Professional Profile ...', 'Nuraly Zhanbyrbayev (@z.nuraly) • Instagram photos and videos',
-        'Нуралы Жанбырбаев | Facebook', 'Нуралы Жанбырбаев | Facebook', 'Nuraly Zhanbyrbayev - Middle .NET Developer - Upwork ...']
+        'Nuraly Zhanbyrbayev - Kazakhstan | Professional Profile ...',
+        'Nuraly Zhanbyrbayev (@z.nuraly) • Instagram photos and videos',
+        'Нуралы Жанбырбаев | Facebook', 'Нуралы Жанбырбаев | Facebook',
+        'Nuraly Zhanbyrbayev - Middle .NET Developer - Upwork ...']
